c31a60412bccdc9fc28a5612a4facbc527236a80
Federico Cofino <federicocofino@Federicos-MacBook-Air.local>
2026-08-10 12:25:19 -0700

    Forgot I should be updating

---
+++ LearningProcess/Step1/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+
+
+const MyBooks = [
+{
+ "title": 'The Hunger Games',
+ "author": 'Suzanne Collins',
+ "id": 'GamesCollins'
+},
+{
+ "title": "Ender's Game",
+ "author": 'Orson Scott Card',
+ "id": 'GameCard'
+},
+{
+ "title": 'Foundation',
+ "author": 'Isaac Asimov',
+ "id": 'FoundationAsimov'
+}
+]
+
+
+function App() {
+  return (
+   <div style={{ textAlign: 'center', marginTop: '50px' }}>
+     <h1>Favorite Book List</h1>
+       <div>
+         <input type = 'text' placeholder='Enter title here...' style ={{margin: '10px'}}/>
+         <input type = 'text' placeholder='Enter author here...'/>
+         <button className='add-button'>Add</button>
+       </div>
+     <ul className='book-list'>
+       {MyBooks.map(book => (
+         <li key={book.id} className='book-item'>
+           {book.title} by {book.author}
+           <button className='del-button'>Delete</button>
+         </li>
+       ))}
+     </ul>
+   </div>
+ )
+}
+
+
+export default App
