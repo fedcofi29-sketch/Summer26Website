37e9f4019ded5cab7ee17f764073c9d006a9a251
Federico Cofino <federicocofino@Federicos-MacBook-Air.local>
2026-08-17 15:24:40 -0700

    sorting not working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,19 +7,19 @@ const myWork = [
   {
     'subject': 'English',
     'task': 'Essay',
-    'due': [8, 24],
+    'due': [8, 24, 2026],
     'id': 1
   },
   {
     'subject': 'Math',
     'task': 'Worksheet',
-    'due': [8, 20],
+    'due': [8, 20, 2026],
     'id': 2
   },
   {
     'subject': 'History',
     'task': 'Annotations',
-    'due': [9, 1],
+    'due': [9, 1, 2026],
     'id': 3
   }
 ]
@@ -44,32 +44,106 @@ function convertToDate(month, day) { // Just learned there is a literal date inp
 
 /* I looked through the inputs and found way more than I expected. Remember to check those in the future.
     Checkbox, submit, date/time could all be useful going forward. 
-      After writing this I then also found dropdowns which are really useful. */
+      After writing that I then also found dropdowns which are really useful. */
 function App() {
+  const [tasks, setTasks] = useState(myWork) // Initial tasks
+  const [inputSubject, setInputSubject] = useState()
+  const [inputAssignment, setInputAssignment] = useState('')
+  const [inputDate, setInputDate] = useState('') // Date passed through as "YYYY-MM-DD" string
+  
+  const handleDateChange = (e) => {
+    const dateString = e.target.value
+    const [year, month, day] = dateString.split('-').map(Number) // Breaks string at each hyphen and converts them to numbers
+    setInputDate([day, month, year])
+  }
+
+  function handleAddClick() {
+    const newHW = {
+      'subject': inputSubject,
+      'task': inputAssignment,
+      'due': inputDate,
+      'id': myWork.length + 1 // id doesn't really matter singe mongoose will eventually make them
+    }
+
+    const newIndex = findNewIndex(newHw)
+    setTasks(tasks.toSpliced(newIndex, 0, newHW))
+    
+}
+  function compareTwoDates(a, b) {
+    if (a < b) {
+      return true
+    } else if (a > b) {
+      return false
+    } else {
+      return 'equal'
+    }
+  }
+
+  function findNewIndex(newHW) {
+    let index
+    for (let i = 0; i < myWork.length-1; i++) {
+      isNewAYearEarlier = compareTwoDates(newHW.due[2], myWork[i].due[2])
+
+      if (isNewAYearEarlier) { // if the new is earlier, it breaks the loop and sets this as the index
+        index = i
+        break
+      } else if (!isNewAYearEarlier) { // if the new is later, it continues to the next item in the list
+        continue
+      } else { // basically the same loop but checking for months
+        // there might be a way to pull this out into another function but I'm not sure how to include breaks and such then
+        isNewAMonthEarlier = compareTwoDates(newHW.due[1], myWork[i].due[1])
+
+        if (isNewAMonthEarlier) { 
+          index = i
+          break
+        } else if (!isNewAMonthrEarlier) {
+          continue
+        } else { // basically the same loop but checking for days
+        
+          isNewADayEarlier = compareTwoDates(newHW.due[1], myWork[i].due[1])
+
+          if (isNewADayEarlier) { 
+            index = i
+            break
+          } else if (!isNewADayrEarlier) {
+            index = i + 1 // so it adds itself to the end if it is the last item
+            continue
+          } else {
+            // if they're due the same day I'm assuming the assingment that was put there first if from an earlier class
+            index = i + 1 
+        }
+      }
+    }
+  }
+  return index
+  }
+
   return (
     <div className="overall-page"> {/* encompasses everything to account for footer */}
       <div style={{flex: '1'}}> {/* this div is everything but the footer */}
       <h1 className='hw-heading'>Homework</h1>
         <div className='input-div'>
-          <select defaultValue="Subject" style={{cursor: 'pointer'}}> 
+          <select defaultValue="Subject" onChange={(e)=>setInputSubject(e.target.value)} style={{cursor: 'pointer'}}> 
             {/* this can be set to have a color based on the selected subjects once states are added to track the value */}
             <option value="Subject" disabled>Subject</option>
             {subjects.map(subject => (
               <option value={subject}>{subject}</option>
             ))}
           </select>
-          <input type = 'string' placeholder = 'Assignment' className='new-assingment-input-dimen'></input>
-          <input type = 'date' className='new-assignment-input-dimen' style={{cursor: 'pointer'}}></input>
-          <button className='add-new-task'>
+          <input type='string' placeholder='Assignment' 
+          className='new-assingment-input-dimen' onChange={(e)=>setAssignment(e.target.value)}></input>
+          <input type='date' className='new-assignment-input-dimen' 
+          style={{cursor: 'pointer'}} onChange={handleDateChange}></input>
+          <button className='add-new-task' onClick={handleAddClick}>
             <img src={downArrow} alt="Add new task"></img>
             </button>
         </div>
         <table className='vertical-lines'> {/* I think it just looks nicer to only have divisions this way */}
         <tbody> {/* Not really sure what tbody does but it gives me an error message if I don't have it */}
-          {myWork.map(work => (
-            <tr key={work.id} style={{backgroundColor:subjectColors[work.subject], color: 'black'}} >
-              <td>{work.task}</td>
-              <td>{convertToDate(work.due[0], work.due[1])}</td>
+          {tasks.map(task => (
+            <tr key={task.id} style={{backgroundColor:subjectColors[task.subject], color: 'black'}} >
+              <td>{task.task}</td>
+              <td>{convertToDate(task.due[0], task.due[1])}</td>
               <td><button className='utility-button'></button></td>
               {/* Not exactly sure what I'm going to use this button for exactly.
                 My intial idea was to have a checkmark to remember which assignments I've finished but haven't submitted yet.
